db0210aba292c51df82e2987adb9c452f19bbbab
Facundo Moya <facundomoya18@hotmail.com>
2021-12-07 10:42:49 -0300

    instalamos nodemon

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import express from "express"
+
+const app = express()
+
+const port = 4000 //elegir un puerto que se que no esta en uso
+
+app.listen(port,()=>{//que escuche o muestre lo que esta pasando en el puerto 4000
+console.log("estoy en el puerto"+port)//no se ejecutan por orden si no por el tiempo que lleva la tarea en ejecutar
+})
 console.log("HOLA MUNDO")
