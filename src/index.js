d3bfb650d9cbcb69bc4dd0f263a74916571e36db
Facundo Moya <facundomoya18@hotmail.com>
2021-12-07 17:35:11 -0300

    se soluciono el problema de nodemon, para esto se volvio a instalar babel, pero de manera global

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import express from "express"
-
+//instanciar express
 const app = express()
+//crear un puerto
+app.set("port",process.env.PORT || 4000) //elegir un puerto que se que no esta en uso
 
-const port = 4000 //elegir un puerto que se que no esta en uso
-
-app.listen(port,()=>{//que escuche o muestre lo que esta pasando en el puerto 4000
-console.log("estoy en el puerto"+port)//no se ejecutan por orden si no por el tiempo que lleva la tarea en ejecutar
+//el app.get trae el contenido de la variable port
+app.listen(app.get("port"),()=>{//que escuche o muestre lo que esta pasando en el puerto 4000
+console.log("estoy en el puerto "+app.get("port"))//no se ejecutan por orden si no por el tiempo que lleva la tarea en ejecutar
 })
-console.log("HOLA MUNDO")
+
