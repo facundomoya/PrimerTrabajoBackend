7f405a89dc575697f9033e01efb65746fec5a5da
Facundo Moya <facundomoya18@hotmail.com>
2021-12-07 18:26:58 -0300

    se vio rutas, peticiones, se añadio middlewares que vamos a usar, morgan y cors, como otros dos que ya vienen con express

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import express from "express"
+import morgan from "morgan"
+import cors from "cors"
+
 //instanciar express
 const app = express()
 //crear un puerto
@@ -9,3 +12,19 @@ app.listen(app.get("port"),()=>{//que escuche o muestre lo que esta pasando en e
 console.log("estoy en el puerto "+app.get("port"))//no se ejecutan por orden si no por el tiempo que lleva la tarea en ejecutar
 })
 
+//configuraciones extras (middlewares), van antes de las rutas
+app.use(morgan("dev")) //dev me da la informacion necesaria de las peticiones, dev es de las palabras mas completas
+app.use(cors())
+//middlewares de express, sirven para que nuestro servidor cuando recibe una peticion remota interprete los archivos de formato json
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))//este si recibe un parametro, ese parametro recibe un objeto y lo interpreta en formato json
+
+//rutas (ruta de prueba) req(request) es la consulta del frontend y res(response) es la respuesta del backend
+app.get("/",(req,res)=>{//localhost:4000
+    res.send("HOLA ESTO ES UNA PRUEBA DESDE EL BACKEND")
+}) //una ruta para cuando alguien me haga una peticion get
+
+//borrar producto (no se puede probar desde el navegador)
+app.delete("/borrarproducto",(req,res)=>{//localhost:4000/borrarproductos
+    res.send("ALGUIEN QUIERE BORRAR")
+}) //una ruta para cuando alguien me haga una peticion get
